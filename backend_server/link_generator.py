271b795b6f4dca0e84d36f45ed7aff625cd78693
Quek Ren Kai <32479799+renkai95@users.noreply.github.com>
2020-08-24 11:25:02 +0800

    Update link_generator.py

--- backend_server/link_generator.py
+++ backend_server/link_generator.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 def generator(link):
     '''
-    Generates a shortened link using a simple hash(salt + link) approach. Result is truncated to 7 chars for readability
+    Generates a shortened link using a simple hash(link + salt) approach. Result is truncated to 7 chars for readability
     :param: link: string
     
 
     '''
     if type(link) == str:
         newString = link + str(getTime())
-        return md5(newString.encode()).hexdigest()[0:8]
+        return md5(newString.encode()).hexdigest()[0:7]
     else:
         raise TypeError
 
